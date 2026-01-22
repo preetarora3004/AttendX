2b03db0c33b87e681397d30c45975378cf5444e8
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-01-22 18:17:03 +0530

    Upgraded syntax

--- packages/utils/src/callback.ts
+++ packages/utils/src/callback.ts
@@ -1,28 +1,34 @@
 import { CallbacksOptions } from "next-auth";
-import {JWT} from 'next-auth/jwt'
+import { JWT } from 'next-auth/jwt'
+import { DefaultSession, Session } from "next-auth";
 
 export const callbacks: Partial<CallbacksOptions> = {
 
-    async jwt({ token, user }) : Promise<JWT>{
+    async jwt({ token, user }): Promise<JWT> {
 
         if (user) {
 
             return {
                 ...token,
-                sub : user.id,
-                username : user.username
+                sub: user.id,
+                username: user.username
             }
         }
 
         return token;
     },
 
-    async session({session, user}){
+    async session({ session, user }): Promise<Session | DefaultSession> {
 
-        if(!session.user || !user) return session;
-
-        session.user.id = user.id,
-        session.user.username = user.username
+        if (session.user || session) {
+            return {
+                ...session,
+                user : {
+                    id : user.id,
+                    username : user.username
+                }
+            }
+        }
 
         return session;
     }
