2b62430f01bad6142a8c56c5d4df3eb96d8c1399
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-01-22 18:17:03 +0530

    Upgraded syntax

--- packages/utils/src/auth.ts
+++ packages/utils/src/auth.ts
@@ -3,7 +3,6 @@ import { providers } from "@workspace/utils/provider"
 import {callbacks} from "@workspace/utils/callback"
 
 export const authOption: NextAuthOptions = {
-
     providers,
     callbacks
 }
