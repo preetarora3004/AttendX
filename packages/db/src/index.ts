6d41ae65075f0f8317fc5a19b3789a8e9b48db98
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-02-01 20:31:15 +0530

    changed schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,10 +3,17 @@ import {PrismaPg} from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
+const url = process.env.DATABASE_URL;
+
+if(!url){
+  throw new Error("DATABASE_URL is missing");
+}
+
 export const client =
   globalForPrisma.prisma ||
   new PrismaClient({
     adapter: new PrismaPg({connectionString : process.env.DATABASE_URL}),
+    log : ["query", "error", "warn"]
   });
 
-if (Bun.env.NODE_ENV !== "production") globalForPrisma.prisma = client;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = client;
--- packages/utils/src/auth.ts
+++ packages/utils/src/auth.ts
@@ -1,6 +1,6 @@
 import { NextAuthOptions } from "next-auth"
 import { providers } from "@workspace/utils/provider"
-import {callbacks} from "@workspace/utils/callback"
+import { callbacks } from "@workspace/utils/callback"
 
 export const authOption: NextAuthOptions = {
     providers,
