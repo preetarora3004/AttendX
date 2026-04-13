585bd473f25f83e2f5f95114c34e60f3abbdfbe4
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-04-13 13:05:49 +0530

    host allow

--- apps/frontend-primary/vite.config.ts
+++ apps/frontend-primary/vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
    plugins: [react(), tailwindcss(), tsconfigPaths()],
    server: {
       host: true,
-      allowedHosts: ["https://attendx-xdzf.onrender.com"],
+      allowedHosts: ["attendx-xdzf.onrender.com"],
    },
 });
