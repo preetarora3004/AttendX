20377450a43312a326adbc3a485f1c0b0d38e122
Preet Arora <141331002+Casper3004@users.noreply.github.com>
2026-04-13 13:00:31 +0530

    Origin

--- apps/frontend-primary/vite.config.ts
+++ apps/frontend-primary/vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
    plugins: [react(), tailwindcss(), tsconfigPaths()],
    server: {
       host: true,
-      allowedHosts: ["unsmooth-freewill-bobtail.ngrok-free.dev"],
+      allowedHosts: ["https://attendx-xdzf.onrender.com"],
    },
 });
